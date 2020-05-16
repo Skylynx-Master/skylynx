0e2c48f45a89f6424c6591dc91cc61ad1268b80e
Jose <jbarriospd@gmail.com>
2020-05-16 14:57:06 -0500

    [Update] configuration mode history in webpack and global styles

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     resolve: {
         extensions: ['.js', '.jsx']
@@ -50,6 +51,9 @@ module.exports = {
                 ]
             }
         ]
+    },
+    devServer: {
+        historyApiFallback: true,
     },
     plugins: [
         new HtmlWebPackPlugin({
