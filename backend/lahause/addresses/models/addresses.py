5f282e4dc9115624fff13ca544cadb608514148b
Carlos Navia <cenaviat@gmail.com>
2020-05-22 10:31:09 -0500

    Refactor model addresses

--- backend/lahause/addresses/models/addresses.py
+++ backend/lahause/addresses/models/addresses.py
@@ -7,7 +7,7 @@ from django.db import models
 from lahause.utils.models import LaHauseModel
 
 
-class City(LaHauseModel):
+class Addresses(LaHauseModel):
     """Addresses model.
 
     Entity that relates the addresses where the properties are
