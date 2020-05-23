47ccfa786cdb49d0e65eab4ae7833e4c6f228caa
Carlos Navia <cenaviat@gmail.com>
2020-05-22 22:31:40 -0500

    definition fields of the models Address

--- backend/lahause/addresses/models/addresses.py
+++ backend/lahause/addresses/models/addresses.py
@@ -7,9 +7,45 @@ from django.db import models
 from lahause.utils.models import LaHauseModel
 
 
-class Addresses(LaHauseModel):
+class Address(LaHauseModel):
     """Addresses model.
 
     Entity that relates the addresses where the properties are
     """
-    pass
+
+    number_building = models.CharField(
+        verbose_name='number_building',
+        max_length=10
+    )
+
+    number_street = models.CharField(
+        verbose_name='number_street',
+        max_length=5
+    )
+
+    area_locality = models.CharField(
+        verbose_name='area_locality',
+        max_length=50
+    )
+
+    zip_postcode = models.CharField(
+        verbose_name='zip_postcode',
+        max_length=10
+    )
+
+    other_address_details = models.CharField(
+        verbose_name='other_address_details',
+        max_length=50
+    )
+
+    city = models.ForeignKey(
+        'cities.City',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        """Return addresses name."""
+        return self.number_building
+
+    REQUIRED_FIELDS = ['number_building', 'number_street','area_locality', 'city' ]
