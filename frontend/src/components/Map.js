ce33ab40014f401d6a34c51aa22646a5853c84a6
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-26 18:56:47 +0000

    Create the function Mapping to render a basic google map with default values, also add a size to the component in order to see it on the page

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import '../assets/styles/components/Map.scss';
+
+
+function Mapping () {
+  return (
+    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 19.5636957, lng: -99.1928209 }} />
+  )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Mapping))
 
 function Map() {
   return(
-    <h1>Hello My Friend Map</h1>
+    <div className="Map">
+      <WrappedMap googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"}
+      loadingElement={<div style={{ height: "0%" }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: "100%" }} />} />
+
+    </div>
   )
 }
 
