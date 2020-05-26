f5db7cbae36cb9df6570817ada5644b42e27b6a0
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-26 18:23:10 +0000

    Create the basic structure of the MAP component and install the react-google-maps library

---
+++ frontend/src/components/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Map() {
+  return(
+    <h1>Hello My Friend Map</h1>
+  )
+}
+
+export default Map;
--- frontend/src/pages/Filters.js
+++ frontend/src/pages/Filters.js
@@ -1,11 +1,14 @@
 import React from "react";
 import SearchAddress from '../components/SearchAddress';
+import Map from '../components/Map';
 
 function Filters () {
   return (
     <div>
       <h1>Hello my friend FILTERS</h1>
       <SearchAddress />
+      <Map />
+
     </div>
     
   );
