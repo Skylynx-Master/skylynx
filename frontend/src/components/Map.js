3dc048f1e3340f2f70529f6ede55348c940cc843
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-26 21:06:46 +0000

    Add the Markers component and simulation info of properties in order to do a map function accross them

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
-import '../assets/styles/components/Map.scss';
+import React from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from "react-google-maps";
+import "../assets/styles/components/Map.scss";
+import * as propertySimulation from "./data/propertySimulation.json";
 
-
-function Mapping () {
+function Mapping() {
   return (
-    <GoogleMap defaultZoom={10} defaultCenter={{ lat: 19.5636957, lng: -99.1928209 }} />
-  )
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 19.5636957, lng: -99.1928209 }}
+    >
+      {propertySimulation.features.map((property) => (
+        <Marker
+          key={property.properties.PARK_ID}
+          position={{
+            lat: property.geometry.coordinates[1],
+            lng: property.geometry.coordinates[0],
+          }}
+        />
+      ))}
+    </GoogleMap>
+  );
 }
 
-const WrappedMap = withScriptjs(withGoogleMap(Mapping))
+const WrappedMap = withScriptjs(withGoogleMap(Mapping));
 
 function Map() {
-  return(
+  return (
     <div className="Map">
-      <WrappedMap googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyDoz2SHkwEIzGjGC61i6YNwY3W036WFFsg&callback=initMap'}
-      loadingElement={<div style={{ height: "0%" }} />}
-          containerElement={<div style={{ height: "100%" }} />}
-          mapElement={<div style={{ height: "100%" }} />} />
-
+      <WrappedMap
+        googleMapURL={
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyDoz2SHkwEIzGjGC61i6YNwY3W036WFFsg&callback=initMap"
+        }
+        loadingElement={<div style={{ height: "0%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
     </div>
-  )
+  );
 }
 
 export default Map;
