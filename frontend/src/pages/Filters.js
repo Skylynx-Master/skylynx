e9d2426a3e802dacada430e435fe136ba6626a5a
Jose Barrios <jbarriospd@gmail.com>
2020-05-26 22:11:56 +0000

    [Add] Services component

--- frontend/src/pages/Filters.js
+++ frontend/src/pages/Filters.js
@@ -1,14 +1,26 @@
 import React from "react";
-import SearchAddress from '../components/SearchAddress';
+import Services from "../components/Services";
+import SearchAddress from "../components/SearchAddress";
 
-function Filters () {
+import "../assets/styles/components/Filters.scss";
+
+function Filters() {
   return (
-    <div>
-      <h1>Hello my friend FILTERS</h1>
-      <SearchAddress />
-    </div>
-    
+    <section className="filters">
+      <div className="filters__ubication">
+        <h1>Ubicacion</h1>
+      </div>
+      <div className="filters__info"></div>
+      <div className="filters__services">
+        <Services />
+        <button type="button" className="button--filter">
+          Filtar
+        </button>
+      </div>
+
+      {/* <SearchAddress /> */}
+    </section>
   );
-};
+}
 
 export default Filters;
