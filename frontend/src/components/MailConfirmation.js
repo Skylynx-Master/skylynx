44ba6f62d5fa3cadededaec84356bbe17f0885c1
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-27 17:44:51 +0000

    Finalized the MailConfirmation component, add styles and remove from the filters page, only for test

--- frontend/src/components/MailConfirmation.js
+++ frontend/src/components/MailConfirmation.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import '../assets/styles/components/MailConfirmation.scss';
+import mailConfirmation from "../assets/static/MailConfirmationIcon.png";
 
-const MailConfirmation = () =>{
-  return(
-    <h1>Hello my friend MailConfirmation</h1>
-  )
-}
+const MailConfirmation = () => {
+  return (
+    <div className="MailConfirmation">
+      <h2>Verifique su correo para confirmar su cuenta</h2>
+      <img src={mailConfirmation} alt="mail confirmation logo"></img>
+    </div>
+  );
+};
 
 export default MailConfirmation;
--- frontend/src/pages/Filters.js
+++ frontend/src/pages/Filters.js
@@ -1,13 +1,10 @@
 import React from "react";
 import SearchAddress from '../components/SearchAddress';
-import Map from '../components/Map';
 
 function Filters () {
   return (
     <div>
-      <h1>Hello my friend FILTERS</h1>
-      {/* <SearchAddress /> */}
-      <Map />
+      <SearchAddress />
 
     </div>
     
