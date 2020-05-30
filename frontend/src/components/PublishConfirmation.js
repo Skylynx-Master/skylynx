06fad9a6e7acfcf87617c0785dd365a116f3acd6
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-30 01:18:58 +0000

    Add styles to the PublishConfirmation component, compare all the modals that will be on the app

--- frontend/src/components/PublishConfirmation.js
+++ frontend/src/components/PublishConfirmation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import checkIcon from '../assets/static/checkIcon.png';
 import {Link} from 'react-router-dom';
+import '../assets/styles/components/PublishConfirmation.scss';
 
 const PublishConfirmation = () =>{
   return(
     <div className="PublishConfirmation">
       
-      <h2>Se ha publicado el inmueble</h2>
+      <h2>Se ha publicado el inmueble!!!</h2>
       <img src={checkIcon}></img>
       <Link to="/dashboard">Regresar a mi tablero</Link>
     </div>
--- frontend/src/pages/Filters.js
+++ frontend/src/pages/Filters.js
@@ -1,12 +1,18 @@
 import React from "react";
 import SearchAddress from "../components/SearchAddress";
 import PublishConfirmation from '../components/PublishConfirmation';
+import AccountConfirmation from '../components/AccountConfirmation';
+import MailConfirmation from '../components/MailConfirmation';
+import SavedPost from '../components/SavedPost';
 
 function Filters() {
   return (
     <div>
       {/* <SearchAddress /> */}
       <PublishConfirmation />
+      <SavedPost />
+      <AccountConfirmation />
+      <MailConfirmation />
     </div>
   );
 }
