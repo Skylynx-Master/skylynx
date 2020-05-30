68e49d3c4e4afd6a0f05a5fd9591cd2ad20e3dc6
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-30 01:47:47 +0000

    Add responsive to the PublishConfirmation component and improve responsive for others components

--- frontend/src/pages/Filters.js
+++ frontend/src/pages/Filters.js
@@ -1,18 +1,10 @@
 import React from "react";
 import SearchAddress from "../components/SearchAddress";
-import PublishConfirmation from '../components/PublishConfirmation';
-import AccountConfirmation from '../components/AccountConfirmation';
-import MailConfirmation from '../components/MailConfirmation';
-import SavedPost from '../components/SavedPost';
 
 function Filters() {
   return (
     <div>
-      {/* <SearchAddress /> */}
-      <PublishConfirmation />
-      <SavedPost />
-      <AccountConfirmation />
-      <MailConfirmation />
+      <SearchAddress />
     </div>
   );
 }
