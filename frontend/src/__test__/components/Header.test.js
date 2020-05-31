0f8338723c2778e4bbfa875668a94e211eab7966
Francisco López <54652030+pacool84@users.noreply.github.com>
2020-05-31 18:44:08 +0000

    Add more test to the header component

--- frontend/src/__test__/components/Header.test.js
+++ frontend/src/__test__/components/Header.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/Header";
 import { mount } from "enzyme";
 import { BrowserRouter } from "react-router-dom";
+import HamburgerMenu from "../../components/HamburgerMenu";
 
 describe("Header Component Testing", () => {
   const header = mount(
@@ -10,15 +11,25 @@ describe("Header Component Testing", () => {
     </BrowserRouter>
   );
 
+  const hamburgerMenu = mount(
+    <BrowserRouter>
+      <HamburgerMenu />
+    </BrowserRouter>
+  );
+
   test("Header Render", () => {
     expect(header.length).toEqual(1);
   });
 
-  test("Header Logo", () => {
+  test("Header Logo Render", () => {
     expect(header.find(".Header-logo").length).toEqual(1);
   });
 
-  test("Header Icons", () => {
+  test("Header Icons and Links Render", () => {
     expect(header.find(".Header-icons").length).toEqual(8);
+  });
+
+  test("Hamburger Menu Availability", () => {
+    expect(hamburgerMenu.length).toEqual(1);
   });
 });
